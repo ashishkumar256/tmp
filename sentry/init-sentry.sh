f65265759d4324f53b33626395d542078df72bfe
Ashish Kumar <ashishkumar@Ashishs-MacBook-Pro.local>
2025-11-12 17:03:16 +0530

    ui

--- sentry/init-sentry.sh
+++ sentry/init-sentry.sh
@@ -11,6 +11,13 @@ sed -i "s|^# CSRF_COOKIE_SECURE = True|CSRF_COOKIE_SECURE = True|" /etc/sentry/s
 cat <<'EOF' >> /etc/sentry/sentry.conf.py
 SENTRY_BEACON = False
 SENTRY_OPTIONS["system.allowed-origins"] = ["https://*.killercoda.com", "http://*.killercoda.com"]
+
+# Allow Django to trust frontend origins for CSRF
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.killercoda.com",
+    "http://*.killercoda.com",
+]
+
 EOF
 
 sentry upgrade --noinput
