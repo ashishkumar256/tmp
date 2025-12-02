c28c2ab45ed5c5a33fc1802d2a3a17a727b2fddd
Ashish Kumar <ashishkumar@Ashishs-MacBook-Pro.local>
2025-12-02 05:52:32 +0530

    source map

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -26,7 +26,9 @@ export default defineConfig({
       url: "https://kumar-gn.sentry.io/",
 
       // 🔖 Release version (important for matching errors to source maps)
-      release: process.env.RELEASE_NAME || process.env.npm_package_version,
+      release: { 
+        name: process.env.VITE_RELEASE_NAME 
+      },
 
       // 🧹 Optional: clean up old source maps before uploading
       cleanArtifacts: true,
